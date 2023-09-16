5ba0179a11ee5491f753ac0b4f650785ec364bab
Zarnigor <umrzakovazarnigor14@gmail.com>
2023-09-16 23:59:47 +0500

    six

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,8 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'photo.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailsPage extends StatefulWidget {
   final Photo photo;
@@ -18,35 +17,35 @@ class _DetailsPageState extends State<DetailsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('${widget.photo.author}', style: const TextStyle(fontSize: 20),),
+        backgroundColor: Colors.red,
+        title: Text('${widget.photo.author}'),
       ),
       body: Column(
         children: [
-           CachedNetworkImage(
-            imageUrl: '${widget.photo.downloadUrl}',
-
-          ),
-          SizedBox(height: 60,),
-          ElevatedButton(
-
-            onPressed: () async {
-              launchUrl(Uri.parse('${widget.photo.url}'));
-            },
-
-            child: Text('Show', style: TextStyle(fontSize: 20,
-
-                color: Colors.black,
-                decoration: TextDecoration.underline),
-
-
+          const SizedBox(height: 50),
+          CachedNetworkImage(imageUrl: '${widget.photo.downloadUrl}'),
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: ElevatedButton(
+              onPressed: () async {
+                launchUrl(Uri.parse('${widget.photo.url}'));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                minimumSize: const Size.fromHeight(56),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+              ),
+              child: const Text(
+                'Original Url',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
             ),
           ),
-
         ],
       ),
-
-
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:http/http.dart' as http;
 import 'details_page.dart';
 import 'photo.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,120 +15,165 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Photo> photos = [];
-  bool isLoading = false;
-  late RefreshController refreshController;
-  int page = 0;
-  bool isupwardEnabled = false;
-  late ScrollController scrollController;
+  List<Photo> _photos = [];
+  bool _isLoading = false;
+  late RefreshController _refreshController;
+  int _page = 0;
+  bool _isUpwardEneble = false;
+  late ScrollController _scrollController;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    refreshController = RefreshController();
-    scrollController = ScrollController();
-    scrollController.addListener(() {
-      if(scrollController.offset>=500) {
+    _initialFetchPhotos();
+    _refreshController = RefreshController();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.offset >= 500) {
         setState(() {
-          isupwardEnabled = true;
+          _isUpwardEneble = true;
         });
-      }
-      else {
+      } else {
         setState(() {
-          isupwardEnabled = false;
+          _isUpwardEneble = false;
         });
       }
     });
-    _initialFetchPhotos();
   }
 
   Future<void> _initialFetchPhotos() async {
     setState(() {
-      isLoading = true;
+      _isLoading = true;
     });
-    final Uri url =
-    Uri.https('picsum.photos', '/v2/list', {'page': '${++page}'});
+    final Uri url = Uri.https(
+      'picsum.photos',
+      '/v2/list',
+      {'page': '${++_page}', 'limit': '15'},
+    );
     final response = await http.get(url);
     final List<dynamic> jsonList = json.decode(response.body);
     setState(() {
-      photos = jsonList.map((json) => Photo.fromJson(json)).toList();
-      isLoading = false;
+      _photos = jsonList.map((json) => Photo.fromJson(json)).toList();
+      _isLoading = false;
     });
   }
 
   Future<void> _moreFetchPhotos() async {
-    final Uri url =
-    Uri.https('picsum.photos', '/v2/list', {'page': '${++page}','limit' : '15'});
+    final Uri url = Uri.https(
+      'picsum.photos',
+      '/v2/list',
+      {'page': '${++_page}', 'limit': '15'},
+    );
     final response = await http.get(url);
     final List<dynamic> jsonList = json.decode(response.body);
     setState(() {
-      photos.addAll(jsonList.map((json) => Photo.fromJson(json)).toList());
+      _photos.addAll(jsonList.map((json) => Photo.fromJson(json)).toList());
     });
-    refreshController.loadComplete();
+    _refreshController.loadComplete();
   }
 
-  Future<void> refrefshPhotos() async {
-    page = 0;
-    final Uri url =
-    Uri.https('picsum.photos', '/v2/list', {'page': '${++page}','limit' : '15'});
+  Future<void> _refreshPhotos() async {
+    _page = 0;
+    final Uri url = Uri.https(
+      'picsum.photos',
+      '/v2/list',
+      {'xpage': '${++_page}', 'limit': '15'},
+    );
     final response = await http.get(url);
     final List<dynamic> jsonList = json.decode(response.body);
     setState(() {
-      photos = jsonList.map((json) => Photo.fromJson(json)).toList();
+      _photos = jsonList.map((json) => Photo.fromJson(json)).toList();
     });
-    refreshController.refreshCompleted();
+    _refreshController.refreshCompleted();
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
-    refreshController.dispose();
-    scrollController.dispose();
+    _refreshController.dispose();
+    _scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: isLoading
-          ? const Center(
-        child: CircularProgressIndicator(
-
-        ),
-      )
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator(color: Colors.red,))
           : SmartRefresher(
-        controller: refreshController,
+        controller: _refreshController,
         enablePullUp: true,
         onLoading: _moreFetchPhotos,
-        onRefresh: refrefshPhotos,
+        onRefresh: _refreshPhotos,
         child: ListView.builder(
-            itemCount: photos.length,
-            controller: scrollController,
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            itemCount: _photos.length,
+            controller: _scrollController,
             itemBuilder: (context, index) {
               return InkWell(
                 onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => DetailsPage(photo: photos[index])));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) =>
+                              DetailsPage(photo: _photos[index])));
                 },
-                child: Column(children: [
-                  // Image.network('${photos[index].downloadUrl}'),
-                  CachedNetworkImage(
-                    imageUrl: '${photos[index].downloadUrl}',
-                    width: double.infinity,
-                    height: 200,
-                    fit: BoxFit.cover,
+                child: Container(
+                  margin: const EdgeInsets.symmetric(
+                      horizontal: 10, vertical: 6),
+                  padding: const EdgeInsets.only(
+                    bottom: 10,
                   ),
-                  Text('${photos[index].author}'),
-                ]),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.red,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                        child: CachedNetworkImage(
+                          imageUrl: '${_photos[index].downloadUrl}',
+                          width: double.infinity,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding:
+                        const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          '${_photos[index].author}',
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               );
             }),
       ),
-      floatingActionButton: isupwardEnabled ? FloatingActionButton(
+      floatingActionButton: _isUpwardEneble
+          ? FloatingActionButton(
         onPressed: () {
-          scrollController.animateTo(0, duration: Duration(milliseconds: 700), curve: Curves.easeInOut);
+          _scrollController.animateTo(
+            0,
+            duration: const Duration(milliseconds: 700),
+            curve: Curves.easeInOut,
+          );
         },
-        child: Icon(Icons.keyboard_arrow_up,size: 40,),
-      ) : null,
+        backgroundColor: Colors.red,
+        child: const Icon(
+          Icons.upgrade,
+          size: 40,
+        ),
+      )
+          : null,
     );
   }
 }
